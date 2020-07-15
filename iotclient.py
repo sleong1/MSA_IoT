d4e0b8569874d9830c5a1eec1161ddb27bb67f22
sleong1 <36063362+sleong1@users.noreply.github.com>
2020-07-15 20:34:50 +1000

    Want to use the client without response for base code

--- iotclient.py
+++ iotclient.py
@@ -1,57 +1,83 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 
-# Using the Python Device SDK for IoT Hub:
-#   https://github.com/Azure/azure-iot-sdk-python
-# The sample connects to a device-specific HTTP endpoint on your IoT Hub.
-import sys
-# pylint: disable=E0611
+"""
+This sample demonstrates how to use the Microsoft Azure Event Hubs Client for Python async API to 
+read messages sent from a device. Please see the documentation for @azure/event-hubs package
+for more details at https://pypi.org/project/azure-eventhub/
 
-from azure.iot.hub import IoTHubRegistryManager
-from azure.iot.hub.models import CloudToDeviceMethod, CloudToDeviceMethodResult
+For an example that uses checkpointing, follow up this sample with the sample in the 
+azure-eventhub-checkpointstoreblob package on GitHub at the following link:
 
-from builtins import input
+https://github.com/Azure/azure-sdk-for-python/blob/master/sdk/eventhub/azure-eventhub-checkpointstoreblob-aio/samples/receive_events_using_checkpoint_store_async.py
+"""
 
-# The service connection string to authenticate with your IoT hub.
-# Using the Azure CLI:
-# az iot hub show-connection-string --hub-name {your iot hub name} --policy-name service
-CONNECTION_STRING = "HostName=su-MSA.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=rCYxF2XMF93L3D3BtG2ZPnGmP88WMrl4qHCoZnLMCKI="
-DEVICE_ID = "MyPythonDevice"
 
-# Details of the direct method to call.
-METHOD_NAME = "SetTelemetryInterval"
-METHOD_PAYLOAD = "10"
+import asyncio
+from azure.eventhub import TransportType
+from azure.eventhub.aio import EventHubConsumerClient
 
-def iothub_devicemethod_sample_run():
+
+# Event Hub-compatible endpoint
+# az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}
+EVENTHUB_COMPATIBLE_ENDPOINT = "{your Event Hubs compatible endpoint}"
+
+# Event Hub-compatible name
+# az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}
+EVENTHUB_COMPATIBLE_PATH = "{your Event Hubs compatible name}"
+
+# Primary key for the "service" policy to read messages
+# az iot hub policy show --name service --query primaryKey --hub-name {your IoT Hub name}
+IOTHUB_SAS_KEY = "{your service primary key}"
+
+# If you have access to the Event Hub-compatible connection string from the Azure portal, then
+# you can skip the Azure CLI commands above, and assign the connection string directly here.
+CONNECTION_STR = f'Endpoint={EVENTHUB_COMPATIBLE_ENDPOINT}/;SharedAccessKeyName=service;SharedAccessKey={IOTHUB_SAS_KEY};EntityPath={EVENTHUB_COMPATIBLE_PATH}'
+
+# Define callbacks to process events
+async def on_event_batch(partition_context, events):
+    for event in events:
+        print("Received event from partition: {}.".format(partition_context.partition_id))
+        print("Telemetry received: ", event.body_as_str())
+        print("Properties (set by device): ", event.properties)
+        print("System properties (set by IoT Hub): ", event.system_properties)
+        print()
+    await partition_context.update_checkpoint()
+
+async def on_error(partition_context, error):
+    # Put your code here. partition_context can be None in the on_error callback.
+    if partition_context:
+        print("An exception: {} occurred during receiving from Partition: {}.".format(
+            partition_context.partition_id,
+            error
+        ))
+    else:
+        print("An exception: {} occurred during the load balance process.".format(error))
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    client = EventHubConsumerClient.from_connection_string(
+        conn_str=CONNECTION_STR,
+        consumer_group="$default",
+        # transport_type=TransportType.AmqpOverWebsocket,  # uncomment it if you want to use web socket
+        # http_proxy={  # uncomment if you want to use proxy 
+        #     'proxy_hostname': '127.0.0.1',  # proxy hostname.
+        #     'proxy_port': 3128,  # proxy port.
+        #     'username': '<proxy user name>',
+        #     'password': '<proxy password>'
+        # }
+    )
     try:
-        # Create IoTHubRegistryManager
-        registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
-
-        # Call the direct method.
-        deviceMethod = CloudToDeviceMethod(method_name=METHOD_NAME, payload=METHOD_PAYLOAD)
-        response = registry_manager.invoke_device_method(DEVICE_ID, deviceMethod)
-
-        print ( "" )
-        print ( "Device Method called" )
-        print ( "Device Method name       : {0}".format(METHOD_NAME) )
-        print ( "Device Method payload    : {0}".format(METHOD_PAYLOAD) )
-        print ( "" )
-        print ( "Response status          : {0}".format(response.status) )
-        print ( "Response payload         : {0}".format(response.payload) )
-
-        input("Press Enter to continue...\n")
-
-    except Exception as ex:
-        print ( "" )
-        print ( "Unexpected error {0}".format(ex) )
-        return
+        loop.run_until_complete(client.receive_batch(on_event_batch=on_event_batch, on_error=on_error))
     except KeyboardInterrupt:
-        print ( "" )
-        print ( "IoTHubDeviceMethod sample stopped" )
+        print("Receiving has stopped.")
+    finally:
+        loop.run_until_complete(client.close())
+        loop.stop()
+    
 
 if __name__ == '__main__':
-    print ( "IoT Hub Python quickstart #2..." )
-    print ( "    Connection string = {0}".format(CONNECTION_STRING) )
-    print ( "    Device ID         = {0}".format(DEVICE_ID) )
-
-    iothub_devicemethod_sample_run()
+    main()
